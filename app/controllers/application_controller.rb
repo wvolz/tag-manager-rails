e30fd047eebd8173a1ccb63f5a8f84656c89691e
Warren Volz <warren@warrenvolz.com>
2018-05-02 12:40:45 -0600

    default timezone to mountain time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception, unless: -> { request.format.json? }
+    #before_action :config_timezone
+
+    private
+
+    #def config_timezone
+    #    Time.zone = 'America/Denver'
+    #end
 end

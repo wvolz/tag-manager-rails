f0492f5cbe72fc0395ca0b29f720e07da25ff717
Warren Volz <warren@warrenvolz.com>
2019-02-28 12:31:34 -0700

    Change PC length in tags to 4 instead of 2
    - 4 characters is the correct length
    - noticed when trying to import records into postgres
      which actually enforced the length requirement

---
+++ db/migrate/20190228191847_change_tag_pc_length.rb
@@ -0,0 +1,8 @@
+class ChangeTagPcLength < ActiveRecord::Migration[5.1]
+  def up 
+      change_column :tags, :pc, :string, limit: 4
+  end
+  def down
+      change column :tags, :pc, :string, limit: 2
+  end
+end

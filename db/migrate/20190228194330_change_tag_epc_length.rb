c48bec14379984f486fb9200f38ef6764faf7f83
Warren Volz <warren@warrenvolz.com>
2019-02-28 12:59:36 -0700

    up tag EPC length up from 24 to 124
    - found by trying to import data from dev to production
      postgres db that enforces the field length
    - EPC max length is 124 based on 18000-6c spec

---
+++ db/migrate/20190228194330_change_tag_epc_length.rb
@@ -0,0 +1,8 @@
+class ChangeTagEpcLength < ActiveRecord::Migration[5.1]
+  def up 
+      change_column :tags, :epc, :string, limit: 124
+  end
+  def down
+      change column :tags, :epc, :string, limit: 24
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190228191847) do
+ActiveRecord::Schema.define(version: 20190228194330) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string "name"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20190228191847) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "epc", limit: 24
+    t.string "epc", limit: 124
     t.string "pc", limit: 4
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

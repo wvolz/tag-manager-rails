06f84c3008a6b3e9da32ad51e0cc6b7379482704
Warren Volz <warren@warrenvolz.com>
2019-12-03 13:11:51 -0700

    only attach tagscan image if it's available
    - logs message if image request fails

--- app/models/tagscan.rb
+++ app/models/tagscan.rb
@@ -5,7 +5,7 @@ class Tagscan < ApplicationRecord
     def tag_epc
         tag.try(:epc)
     end
-    
+
     def tag_pc
         tag.try(:pc)
     end
@@ -26,30 +26,36 @@ class Tagscan < ApplicationRecord
         uri = URI.parse(url)
         uri.user = Rails.application.credentials.grab_photo[:username]
         uri.password = Rails.application.credentials.grab_photo[:password] 
-        
+
         h = Net::HTTP.new uri.host, uri.port
-        
+ 
         req = Net::HTTP::Get.new uri.request_uri
-        
+
         res = h.request req
-        
+
         digest_auth = Net::HTTP::DigestAuth.new
         auth = digest_auth.auth_header uri, res['www-authenticate'], 'GET'
-        
+ 
         req = Net::HTTP::Get.new uri.request_uri
         req.add_field 'Authorization', auth
-        
+
         res = h.request req
-       
-        Tempfile.open('tagscan', :binmode => '1') do |f|
-          f.write(res.body)
-          # ensure file is written to disk
-          f.flush
-          # reset ios to beginning of file
-          f.rewind
-          self.image.attach(io: f,
-                            filename: 'image.jpg',
-                            content_type: 'image/jpg')
+
+        # check request result here, if not 200 don't attach anything
+        if res.is_a?(Net::HTTPSuccess)
+          Tempfile.open('tagscan', :binmode => '1') do |f|
+            f.write(res.body)
+            # ensure file is written to disk
+            f.flush
+            # reset ios to beginning of file
+            f.rewind
+            self.image.attach(io: f,
+                              filename: 'image.jpg',
+                              content_type: 'image/jpg')
+          end
+        else
+          # log issue grabbing image
+          logger.info "Couldn't grab image from #{url} code #{res.code} msg #{res.msg}"
         end
     end
 end

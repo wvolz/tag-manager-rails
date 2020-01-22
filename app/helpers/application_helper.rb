6aa4e7f607aab3dd7e0e45441bb1a2627c2206db
Warren Volz <warren@warrenvolz.com>
2020-01-22 16:12:11 -0700

    Correct fontawesome imports and namespace for chevron

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to({:sort => column, :direction => direction}, {:class=> css_class}) do
-      "#{title} #{content_tag(:i, '', :class=>"fa fa-chevron-#{direction=='asc'?'up':'down'}", :style=>"font-size: 6px;") if css_class.present?}".html_safe
+      "#{title} #{content_tag(:i, '', :class=>"fas fa-chevron-#{direction=='asc'?'up':'down'}", :style=>"font-size: 6px;") if css_class.present?}".html_safe
     end
   end
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
-import '@fortawesome/fontawesome-free/scss/regular.scss';
-import '@fortawesome/fontawesome-free/scss/solid.scss';
+import "@fortawesome/fontawesome-free/js/all";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

0360565c0495854a248824b544a21b4e5727514e
Warren Volz <warren@warrenvolz.com>
2020-02-03 15:49:55 -0700

    Order last 20 tagscans by created_at descending
    - Add scope to tagscan to order by created_at descending

--- app/models/tagscan.rb
+++ app/models/tagscan.rb
@@ -2,6 +2,7 @@ class Tagscan < ApplicationRecord
     belongs_to :tag
     has_one_attached :image
     after_create :update_last_seen_time
+    scope :by_created, -> { order(created_at: :desc) }
 
     def tag_epc
         tag.try(:epc)

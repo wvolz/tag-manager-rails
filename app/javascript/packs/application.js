97d7e1cad3384bd47f0339fcd8b518cb6f24fa06
Warren Volz <warren@warrenvolz.com>
2020-02-03 15:51:20 -0700

    fix localtime by adding local-time to app javascript
    - broke after switch to webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,9 @@ require("channels")
 
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
+import LocalTime from "local-time"
+LocalTime.start()
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.

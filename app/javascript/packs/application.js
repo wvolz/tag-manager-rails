a9fc2de4f2c66c11568cff19c69c2f38a81bcbe9
Warren Volz <warren@warrenvolz.com>
2021-04-07 14:50:18 -0600

    bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,10 +16,8 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import LocalTime from "local-time"
 LocalTime.start()
+import 'bootstrap'
+import { createPopper } from "@popperjs/core";
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// CSS
+import 'scss/site'

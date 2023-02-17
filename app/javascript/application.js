564b9c2de8d25c2296bdca46b61f8d8027272e14
Warren Volz <warren@warrenvolz.com>
2023-02-17 16:33:49 -0700

    esbuild, remove webpack, updated css/js includes + packages

---
+++ app/javascript/application.js
@@ -0,0 +1,11 @@
+// Entry point for the build script in your package.json
+import "./controllers"
+
+import "@fortawesome/fontawesome-free/js/fontawesome"
+import "@fortawesome/fontawesome-free/js/solid"
+import * as bootstrap from "bootstrap"
+//import { createPopper } from "@popperjs/core"
+import { LocalTime } from 'local-time'
+
+
+LocalTime.start()

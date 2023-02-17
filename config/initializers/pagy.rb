afd934ee64f2f6f773e3cd11ade6b4078a40ab36
Warren Volz <warren@warrenvolz.com>
2023-02-17 16:52:25 -0700

    pagy bootstrap, switch to combonavjs

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -14,7 +14,7 @@
 # Instance variables
 # See https://ddnexus.github.io/pagy/docs/api/pagy#instance-variables
 # Pagy::DEFAULT[:page]   = 1                                  # default
-Pagy::DEFAULT[:items]  = 30                                 # default
+Pagy::DEFAULT[:items]  = 30                                   # default
 # Pagy::DEFAULT[:outset] = 0                                  # default
 
 
@@ -127,7 +127,7 @@ Pagy::DEFAULT[:items]  = 30                                 # default
 
 # Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
 # See https://ddnexus.github.io/pagy/docs/extras/bootstrap
-# require 'pagy/extras/bootstrap'
+require 'pagy/extras/bootstrap'
 
 # Bulma extra: Add nav, nav_js and combo_nav_js helpers and templates for Bulma pagination
 # See https://ddnexus.github.io/pagy/docs/extras/bulma

59d6af229adad7adaaffc58343dc5f28c9c2235c
Warren Volz <warren@warrenvolz.com>
2023-02-18 15:47:05 -0700

    Correctly enable turbo / turbo-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "./controllers"
 
 import "@fortawesome/fontawesome-free/js/fontawesome"
 import "@fortawesome/fontawesome-free/js/solid"
+import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
 //import { createPopper } from "@popperjs/core"
 import { LocalTime } from 'local-time'

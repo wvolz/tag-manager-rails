794893896502cf39a0cbbaa9561d5cfdb2ab07b1
Warren Volz <warren@warrenvolz.com>
2026-06-16 11:02:17 -0600

    GS1-SGTIN decoding

--- app/models/tag_type.rb
+++ app/models/tag_type.rb
@@ -1,7 +1,7 @@
 class TagType < ApplicationRecord
   has_many :tags, dependent: :destroy
 
-  enum :decoder, { None: 0, E470: 1 }
+  enum :decoder, { None: 0, E470: 1, GS1_SGTIN: 2 }
 
   class Classification < BinData::Record
     bit1 :vehicleClass
@@ -21,15 +21,68 @@ class TagType < ApplicationRecord
     bit16 :uiiValidationHash
   end
 
-  def uii_decode(epc)
-    if @parsed_uii
-      @parsed_uii
-    elsif decoder == "E470"
-      unless epc.nil?
-        indata_array = [ epc ].pack("H*").bytes
-        indata = indata_array.pack("C*")
-        @parsed_uii = SIXCTOCUII.read(indata)
-      end
+  class GS1SGTIN96
+    attr_reader :header, :filter, :partition, :company_prefix, :item_reference, :serial
+
+    def initialize(header:, filter:, partition:, company_prefix:, item_reference:, serial:)
+      @header = header
+      @filter = filter
+      @partition = partition
+      @company_prefix = company_prefix
+      @item_reference = item_reference
+      @serial = serial
     end
+  end
+
+  def uii_decode(epc)
+    return @parsed_uii if @parsed_uii
+
+    if decoder == "E470" && epc.present?
+      indata_array = [ epc ].pack("H*").bytes
+      indata = indata_array.pack("C*")
+      @parsed_uii = SIXCTOCUII.read(indata)
+    elsif decoder == "GS1_SGTIN" && epc.present?
+      @parsed_uii = decode_gs1_sgtin(epc)
+    end
+
+    @parsed_uii
+  end
+
+  private
+
+  def decode_gs1_sgtin(epc)
+    value = epc.to_s.hex
+    bits = value.to_s(2).rjust(96, "0")
+
+    header = bits[0, 8].to_i(2)
+    filter = bits[8, 3].to_i(2)
+    partition = bits[11, 3].to_i(2)
+
+    company_prefix_bits, item_reference_bits = gs1_partition_layout(partition)
+    company_prefix = bits[14, company_prefix_bits].to_i(2)
+    item_reference = bits[14 + company_prefix_bits, item_reference_bits].to_i(2)
+    serial = bits[58, 38].to_i(2)
+
+    GS1SGTIN96.new(
+      header: header,
+      filter: filter,
+      partition: partition,
+      company_prefix: company_prefix,
+      item_reference: item_reference,
+      serial: serial
+    )
+  end
+
+  def gs1_partition_layout(partition)
+    {
+      0 => [40, 4],
+      1 => [37, 7],
+      2 => [34, 10],
+      3 => [30, 14],
+      4 => [27, 17],
+      5 => [24, 20],
+      6 => [20, 24],
+      7 => [17, 27]
+    }.fetch(partition) { [40, 4] }
   end
 end
--- test/models/tag_type_test.rb
+++ test/models/tag_type_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class TagTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "decodes GS1-SGTIN EPC metadata" do
+    tag_type = TagType.new(decoder: :GS1_SGTIN)
+
+    epc = format("%096x", (0x30 << 88) | (0x1 << 85) | (0x3 << 82) |
+      (0x12345 << 52) | (0x1234 << 38) | 0x123456789)
+
+    decoded = tag_type.uii_decode(epc)
+
+    assert_equal 48, decoded.header
+    assert_equal 1, decoded.filter
+    assert_equal 3, decoded.partition
+    assert_equal 0x12345, decoded.company_prefix
+    assert_equal 0x1234, decoded.item_reference
+    assert_equal 0x123456789, decoded.serial
+  end
 end
